7b648c7f1d94810dbf5ac45e8059c53b658802fb
MIJANURrrrr <mrmijanur816@gmail.com>
2025-04-06 11:09:59 +0530

    Add files via upload

---
+++ sq 10 re.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int sumofsquare (int)
+int main()
+{
+	int a;
+	printf("enter a=");
+	scanf("%d",&a);
+	printf( "%d","sum of square(a)");
+	return 0;
+} int sumofsquare (int n);
+{
+int sum=0 ;
+for (int i=1 ;i<=n;i++)}
+{
+	sum=sum +(i*i)}
+	return sum ;
+}
